850fa8ea8626f5a1bd15d8fcabef08ed33f674fd
Vladymyr <hoghord@gmail.com>
2022-06-07 19:29:11 +0300

    Nested Loops JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 "use strict";
- 
-let num = 10;
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
+// let result = "";
+// const lenght = 7;
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// for (let i = 1; i < lenght; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
 // }
 
-// do {
-//     console.log("zibert");
-//     num++;
-// } while(num < 55);
+// console.log(result);
 
-for (let i = 0; i < 10; i++) {
-
-    if (i % 2 == 0) {continue;}
-
-    console.log(i);
-    
+first: for (let i = 1; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 5; k++) {
+            if (k == 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
 }
