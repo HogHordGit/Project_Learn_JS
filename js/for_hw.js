2e4ccce4197503067a9dc343a921bb8aaa03f2ea
Vladymyr <hoghord@gmail.com>
2022-06-07 22:25:09 +0300

    Done HW number 2

---
+++ js/for_hw.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let flag = true;
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    flag = true;
+
+    const questLastFilm = prompt("Один из последних просмотренных фильмов?");
+    const questMarkFilm = prompt("На сколько оцените его?");
+
+    if (questLastFilm != null && questMarkFilm != null && questLastFilm != "" && 
+    questMarkFilm != "" && questLastFilm.length < 50) {
+        personalMovieDB.movies[questLastFilm] = questMarkFilm;
+        console.log("Date has been added");
+    } else {
+        console.log("Error with data");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман!");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
