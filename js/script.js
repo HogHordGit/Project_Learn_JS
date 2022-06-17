54b79d91111f7f6d6c9653d19e3b272f03bb52a5
HogHordGit <hoghord@gmail.com>
2022-06-17 11:29:20 +0300

    Arrays and objects

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 'use strict';
+// 0.27 Массивы
+// const arr = [2, 13, 26, 8, 5];
 
-const arr = [2, 13, 26, 8, 5];
+// arr.sort(compareNum);
+// console.log(arr);
 
-arr.sort(compareNum);
-console.log(arr);
-
-function compareNum(a, b) {
-    return a - b;
-}
+// function compareNum(a, b) {
+//     return a - b;
+// }
 
 // arr[99] = 0;
 // console.log(arr.length);
@@ -28,10 +28,111 @@ function compareNum(a, b) {
 //     console.log(key);
 // }
 
-const str = prompt("", "");
-const products = str.split(", ");
-products.sort();
-console.log(products.join("; "));
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));
+
+// -------------------------------------------------------------------------
+
+// 0.29 Копирование объектов
+
+// let a = 5;
+// let b = a;
+
+// b = b + 5;
+
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // Ссылка на объект
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// // console.log(newNumbers);
+// // console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add);
+
+// clone.d = 20;
+
+// // console.log(add);
+// // console.log(clone);
+
+// const oldArray = ["a", "b", "c"];
+// const newArray = oldArray.slice();
+
+// newArray[1] = "aa";
+
+// // console.log(oldArray);
+// // console.log(newArray);
+
+// const video = ["youtube", "vimeo", "rutube"];
+// const blogs = ["wordpress", "livejournal", "blogger"];
+// const internet = [...video, ...blogs, "vk", "facebook"];
+
+// // console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newArrayd = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObject = {...q};
+
+// console.log(newObject);
 
 
 
