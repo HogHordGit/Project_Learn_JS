25071f9a9b9fb94932793b007f88ca0c95793df4
Vladymyr <hoghord@gmail.com>
2022-07-07 19:43:44 +0300

    Add new folder

---
+++ Additional sections/Events/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const btns = document.querySelectorAll("button"),
+      overlay = document.querySelector(".overlay");
+
+
+// btn.onclick = function() {
+//     alert("Click");
+// };
+
+// btn.onclick = function() {
+//     alert("Zibert");
+// };
+
+// let i = 0;
+const deleteElement = (event) => {
+    console.log(event.currentTarget);
+    console.log(event.type);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener("click", deleteElement);
+    // }
+};
+
+// btn.addEventListener("click", deleteElement);
+// overlay.addEventListener("click", deleteElement);
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", deleteElement, {once: true});
+});
+
+const link = document.querySelector("a");
+
+link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+});
